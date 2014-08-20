823d13adfb52e0fde700beb890f86e3e761a93c1
Thibault Jouan <tj@a13.fr>
2014-08-20 20:13:11 +0000

    Remove dead code

--- ext/holo/holo.c
+++ ext/holo/holo.c
@@ -18,10 +18,4 @@ void Init_holo(void) {
   rb_define_method(cDisplay, "initialize", display_init, -1);
   rb_define_method(cDisplay, "open", display_open, 0);
   rb_define_method(cDisplay, "close", display_close, 0);
-
-  mXlib = rb_define_module_under(rb_define_module("Holo"), "Xlib");
-
-  cXlibDisplay = rb_define_class_under(mXlib, "Display", rb_cObject);
-  rb_define_alloc_func(cXlibDisplay, xlib_display_alloc);
-  rb_define_method(cXlibDisplay, "close", xlib_display_close, 0);
 }
--- ext/holo/holo.h
+++ ext/holo/holo.h
@@ -17,12 +17,8 @@ VALUE eHoloDisplayError;
 
 VALUE display_alloc(VALUE klass);
 VALUE display_init(int ac, VALUE *av, VALUE self);
-VALUE display_close(VALUE self);
-
-VALUE xlib_display_alloc(VALUE klass);
-VALUE xlib_display_init(int ac, VALUE *av, VALUE self);
 VALUE display_open(VALUE self);
-VALUE xlib_display_close(VALUE self);
+VALUE display_close(VALUE self);
 
 
 #endif
