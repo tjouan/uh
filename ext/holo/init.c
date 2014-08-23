87c404e63a509bba34009f0c48c09cd141b360fc
Thibault Jouan <tj@a13.fr>
2014-08-23 13:41:53 +0000

    Fix indentation

--- ext/holo/init.c
+++ ext/holo/init.c
@@ -29,7 +29,7 @@ void holo_display() {
   rb_define_method(cDisplay, "listen_events", display_listen_events, 0);
   rb_define_method(cDisplay, "sync", display_sync, 1);
   rb_define_method(cDisplay, "change_window_attributes",
-      display_change_window_attributes, 0);
+    display_change_window_attributes, 0);
   rb_define_method(cDisplay, "grab_key", display_grab_key, 1);
 }
 
