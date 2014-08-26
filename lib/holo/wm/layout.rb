6f9dc242170f1f32a303dfd090aeeaefdbb63a2a
Thibault Jouan <tj@a13.fr>
2014-08-26 14:46:12 +0000

    Extract Tag

--- lib/holo/wm/layout.rb
+++ lib/holo/wm/layout.rb
@@ -1,54 +1,7 @@
 module Holo
   class WM
     class Layout
-      class Tag
-        attr_reader :id, :clients, :current_client
-
-        def initialize(id, clients = [])
-          @id             = id
-          @clients        = clients
-          @current_client = nil
-        end
-
-        def current_client_index
-          clients.index current_client
-        end
-
-        def visible_clients
-          [current_client].compact
-        end
-
-        def include?(client)
-          clients.include? client
-        end
-
-        def <<(client)
-          clients << client
-          @current_client = client
-        end
-
-        def remove(client)
-          clients.reject! { |e| e == client}
-          @current_client = clients.last
-        end
-
-        def sel_next
-          sel :succ
-        end
-
-        def sel_prev
-          sel :pred
-        end
-
-
-        private
-
-        def sel(direction)
-          new_index = current_client_index.send direction
-          @current_client = clients[new_index % clients.size]
-        end
-      end
-
+      require 'holo/wm/layout/tag'
 
       attr_reader :manager, :tags, :current_tag
 
