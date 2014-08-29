176e170e48f2393d35956bca99f0555ee87da573
Thibault Jouan <tj@a13.fr>
2014-08-29 01:47:03 +0000

    Refactor Geo as a Struct

--- lib/holo/geo.rb
+++ lib/holo/geo.rb
@@ -1,14 +1,5 @@
 module Holo
-  class Geo
-    attr_accessor :x, :y, :width, :height
-
-    def initialize(x = 0, y = 0, width = 484, height = 300)
-      @x      = x
-      @y      = y
-      @width  = width
-      @height = height
-    end
-
+  class Geo < Struct.new(:x, :y, :width, :height)
     def to_s
       '%dx%d+%d+%d' % [width, height, x, y]
     end
