8ec047ce3f66ab3976fe0af3811012cd0ba7685b
Thibault Jouan <tj@a13.fr>
2014-08-30 00:25:34 +0000

    Fix Geo#to_s nil attributes handling

--- lib/holo/geo.rb
+++ lib/holo/geo.rb
@@ -1,7 +1,12 @@
 module Holo
   class Geo < Struct.new(:x, :y, :width, :height)
     def to_s
-      '%dx%d+%d+%d' % [width, height, x, y]
+      '%sx%s+%s+%s' % [
+        width   ? width.to_s  : ??,
+        height  ? height.to_s : ??,
+        x       ? x.to_s      : ??,
+        y       ? y.to_s      : ??
+      ]
     end
   end
 end
