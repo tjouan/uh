d23503168d9177cd0286640d1417a9e4513db97c
Thibault Jouan <tj@a13.fr>
2014-08-30 19:55:11 +0000

    Use Fixnum to store font ascent and descent

--- ext/holo/font.c
+++ ext/holo/font.c
@@ -6,8 +6,8 @@ VALUE font_make(int ascent, int descent) {
   VALUE args[0];
 
   obj = rb_class_new_instance(0, args, cFont);
-  rb_ivar_set(obj, rb_intern("@ascent"), INT2NUM(ascent));
-  rb_ivar_set(obj, rb_intern("@descent"), INT2NUM(descent));
+  rb_ivar_set(obj, rb_intern("@ascent"), INT2FIX(ascent));
+  rb_ivar_set(obj, rb_intern("@descent"), INT2FIX(descent));
 
   return obj;
 }
