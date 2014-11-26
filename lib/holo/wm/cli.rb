da52decc00d483e98f881195271f714e2f5afc75
Thibault Jouan <tj@a13.fr>
2014-11-26 19:34:27 +0000

    Allow WM to be configured with a run control file

--- lib/holo/wm/cli.rb
+++ lib/holo/wm/cli.rb
@@ -1,14 +1,36 @@
 module Holo
   class WM
     class CLI
+      RC_PATH = "#{ENV['HOME']}/.holo/config.rb".freeze
+
       class << self
         def run!(arguments)
           new.run
         end
       end
 
+      def initialize(wm: nil, rc_path: RC_PATH)
+        @wm       = wm
+        @rc_path  = rc_path
+      end
+
       def run
-        WM.default.run
+        wm.run
+      end
+
+      def wm
+        @wm ||= if FileTest.readable? @rc_path
+          configure_wm
+        else
+          WM.default
+        end
+      end
+
+
+      private
+
+      def configure_wm
+        WM.new.tap { |o| o.instance_eval File.read(@rc_path), @rc_path }
       end
     end
   end
