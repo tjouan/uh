09871d7900b9f39273ce3083a3b9352cb470af80
Thibault Jouan <tj@a13.fr>
2014-11-27 03:06:39 +0000

    Revert "Allow WM to be configured with a run control file"
    This reverts commit ca4710c9eee23774e20bde8594871a508cb2cb39.

--- lib/holo/wm/cli.rb
+++ lib/holo/wm/cli.rb
@@ -1,36 +1,14 @@
 module Holo
   class WM
     class CLI
-      RC_PATH = "#{ENV['HOME']}/.holo/config.rb".freeze
-
       class << self
         def run!(arguments)
           new.run
         end
       end
 
-      def initialize(wm: nil, rc_path: RC_PATH)
-        @wm       = wm
-        @rc_path  = rc_path
-      end
-
       def run
-        wm.run
-      end
-
-      def wm
-        @wm ||= if FileTest.readable? @rc_path
-          configure_wm
-        else
-          WM.default
-        end
-      end
-
-
-      private
-
-      def configure_wm
-        WM.new.tap { |o| o.instance_eval File.read(@rc_path), @rc_path }
+        WM.default.run
       end
     end
   end
