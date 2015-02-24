5c8a5b5c1af5c77b7e008822276c5fe4c7843636
Thibault Jouan <tj@a13.fr>
2015-02-24 05:02:08 +0000

    Improve log messages in WM::Client

--- lib/holo/wm/client.rb
+++ lib/holo/wm/client.rb
@@ -21,38 +21,38 @@ module Holo
       end
 
       def configure
-        puts '  %s.configure %s' % [self.class, self]
+        puts '  %s#configure %s' % [self.class, self]
         window.configure geo
         self
       end
 
       def moveresize
-        puts '  %s.moveresize %s' % [self.class, self]
+        puts '  %s#moveresize %s' % [self.class, self]
         window.moveresize geo
         self
       end
 
       def show
-        puts '  %s.show %s' % [self.class, self]
+        puts '  %s#show %s' % [self.class, self]
         window.map
         self
       end
 
       def hide
-        puts '  %s.hide %s' % [self.class, self]
+        puts '  %s#hide %s' % [self.class, self]
         window.unmap
         self
       end
 
       def focus
-        puts '  %s.{raise,focus} %s' % [self.class, self]
+        puts '  %s#{raise,focus} %s' % [self.class, self]
         window.raise
         window.focus
         self
       end
 
       def kill
-        puts '  %s.kill %s' % [self.class, self]
+        puts '  %s#kill %s' % [self.class, self]
         window.kill
         self
       end
