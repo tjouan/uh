9c17b2e02e9e1e785a6f77254d40e625ecb89f34
Thibault Jouan <tj@a13.fr>
2015-02-26 14:12:10 +0000

    Remove Client standard output debugging messages

--- lib/holo/wm/client.rb
+++ lib/holo/wm/client.rb
@@ -21,38 +21,32 @@ module Holo
       end
 
       def configure
-        puts '  %s#configure %s' % [self.class, self]
         window.configure geo
         self
       end
 
       def moveresize
-        puts '  %s#moveresize %s' % [self.class, self]
         window.moveresize geo
         self
       end
 
       def show
-        puts '  %s#show %s' % [self.class, self]
         window.map
         self
       end
 
       def hide
-        puts '  %s#hide %s' % [self.class, self]
         window.unmap
         self
       end
 
       def focus
-        puts '  %s#{raise,focus} %s' % [self.class, self]
         window.raise
         window.focus
         self
       end
 
       def kill
-        puts '  %s#kill %s' % [self.class, self]
         if window.icccm_wm_protocols.include? :WM_DELETE_WINDOW
           window.icccm_wm_delete
         else
@@ -62,7 +56,6 @@ module Holo
       end
 
       def kill!
-        puts '  %s#kill! %s' % [self.class, self]
         window.kill
         self
       end
