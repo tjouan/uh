bc68ceb157bf1317bc0a863f647794e5faa821c8
Thibault Jouan <tj@a13.fr>
2015-03-21 04:01:15 +0000

    Accept a timeout callback in MultiplexingWorker
    * Modify #configure to accept callback as on_timer keyword;
    * Flush the output buffer before each call to select, not only on
      #setup, so any event generated by the timeout callback is handled
      immediately (else it would have to wait until the next call to
      Display#next_event).

--- lib/uh/wm/workers/multiplexing_worker.rb
+++ lib/uh/wm/workers/multiplexing_worker.rb
@@ -4,23 +4,27 @@ module Uh
       class MultiplexingWorker < BaseWorker
         DEFAULT_TIMEOUT = 4
 
-        def configure(timeout: DEFAULT_TIMEOUT)
-          @timeout = timeout
+        def configure(timeout: DEFAULT_TIMEOUT, on_timeout: nil)
+          @timeout    = timeout
+          @on_timeout = on_timeout
         end
 
         def setup
           @display_io = IO.new(@display.fileno)
-          @display.flush
           self
         end
 
         def each_event
           loop do
+            @display.flush
             @logger.debug "select [#{@display_io.fileno}], _, _, #{@timeout}"
             rs, _ = IO.select [@display_io], [], [], @timeout
             @logger.debug " => #{rs.inspect}"
-            next if rs.nil? || rs.empty?
-            yield @display.next_event while @display.pending?
+            if rs.nil? || rs.empty?
+              @on_timeout.call if @on_timeout
+            else
+              yield @display.next_event while @display.pending?
+            end
           end
         end
       end
