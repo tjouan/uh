6e1351f395a21d4ae2093f0fbd58b5579db68e1a
Thibault Jouan <tj@a13.fr>
2015-04-26 22:07:00 +0000

    Embed tests in Uh namespace

--- test/uh/test_geo.rb
+++ test/uh/test_geo.rb
@@ -1,51 +1,53 @@
 require 'test_helper'
 
-describe Uh::Geo do
-  subject { Uh::Geo.new(0, 0, 640, 480) }
+module Uh
+  describe Geo do
+    subject { Geo.new(0, 0, 640, 480) }
 
-  describe '.new' do
-    it 'raises error when invalid width is given' do
-      assert_raises(Uh::ArgumentError) { Uh::Geo.new(0, 0, 0, 480) }
-    end
-
-    it 'raises error when invalid height is given' do
-      assert_raises(Uh::ArgumentError) { Uh::Geo.new(0, 0, 640, 0) }
-    end
-
-    it 'builds a geo without arguments' do
-      _(Uh::Geo.new).must_be_instance_of Uh::Geo
-    end
-  end
-
-  describe '.format_xgeometry' do
-    it 'formats coordinates and dimensions as X geometry' do
-      _(Uh::Geo.format_xgeometry(0, 0, 640, 480)).must_equal '640x480+0+0'
-    end
-
-    it 'formats missing values as ?' do
-      _(Uh::Geo.format_xgeometry(*[nil] * 4)).must_equal '?x?+?+?'
-    end
-  end
-
-  describe '#to_s' do
-    it 'returns .format_xgeometry results' do
-      _(subject.to_s).must_equal Uh::Geo.format_xgeometry(*subject.values)
-    end
-  end
-
-  %w[width height].each do |dimension|
-    describe "##{dimension}=" do
-      it 'assigns given value' do
-        subject.send "#{dimension}=", 42
-        _(subject.send dimension).must_equal 42
+    describe '.new' do
+      it 'raises error when invalid width is given' do
+        assert_raises(ArgumentError) { Geo.new(0, 0, 0, 480) }
       end
 
-      it 'raises error when 0 is given' do
-        assert_raises(Uh::ArgumentError) { subject.send "#{dimension}=", 0 }
+      it 'raises error when invalid height is given' do
+        assert_raises(ArgumentError) { Geo.new(0, 0, 640, 0) }
       end
 
-      it 'raises error when negative value is given' do
-        assert_raises(Uh::ArgumentError) { subject.send "#{dimension}=", -42 }
+      it 'builds a geo without arguments' do
+        _(Geo.new).must_be_instance_of Geo
+      end
+    end
+
+    describe '.format_xgeometry' do
+      it 'formats coordinates and dimensions as X geometry' do
+        _(Geo.format_xgeometry(0, 0, 640, 480)).must_equal '640x480+0+0'
+      end
+
+      it 'formats missing values as ?' do
+        _(Geo.format_xgeometry(*[nil] * 4)).must_equal '?x?+?+?'
+      end
+    end
+
+    describe '#to_s' do
+      it 'returns .format_xgeometry results' do
+        _(subject.to_s).must_equal Geo.format_xgeometry(*subject.values)
+      end
+    end
+
+    %w[width height].each do |dimension|
+      describe "##{dimension}=" do
+        it 'assigns given value' do
+          subject.send "#{dimension}=", 42
+          _(subject.send dimension).must_equal 42
+        end
+
+        it 'raises error when 0 is given' do
+          assert_raises(ArgumentError) { subject.send "#{dimension}=", 0 }
+        end
+
+        it 'raises error when negative value is given' do
+          assert_raises(ArgumentError) { subject.send "#{dimension}=", -42 }
+        end
       end
     end
   end
