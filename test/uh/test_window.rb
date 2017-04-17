a3bc9c63efcb7a99d2c9101acce6f8f8cac6fd10
Thibault Jouan <tj@a13.fr>
2017-04-17 13:23:19 +0000

    Test Window#==

--- test/uh/test_window.rb
+++ test/uh/test_window.rb
@@ -11,6 +11,19 @@ module Uh; class UhWindowSpec < UhSpec
       end
     end
 
+    describe '#==' do
+      let(:geo)           { Geo.new(0, 0, 320, 240) }
+      let(:window_other)  { dpy.create_window geo }
+
+      it 'returns true when given same window' do
+        _(subject.== subject).must_equal true
+      end
+
+      it 'returns false when given another window' do
+        _(subject.== window_other).must_equal false
+      end
+    end
+
     describe '#name=' do
       it 'raises an error when given argument is not a string' do
         assert_raises(TypeError) { subject.name = :not_a_string }
