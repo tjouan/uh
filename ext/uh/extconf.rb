bac2d1806b6f4b63c46c9129a24bbeff6c7afba8
Thibault Jouan <tj@a13.fr>
2017-04-17 12:41:26 +0000

    Simplify debug build

--- ext/uh/extconf.rb
+++ ext/uh/extconf.rb
@@ -3,7 +3,7 @@ require 'mkmf'
 %w[X11 Xinerama].each { |e| fail "lib#{e} is required" unless have_library e }
 
 $CFLAGS << ' -std=c99 -Wall'
-if %w[DEBUG VALGRIND].any? { |e| ENV.key? e }
+if ENV.key? 'DEBUG'
   $CFLAGS.gsub! /-O\d\s/, '-g '
 end
 
