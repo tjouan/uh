8c18e688ab14540f3353017eaece6e32902f3ad5
Thibault Jouan <tj@a13.fr>
2017-04-17 13:20:22 +0000

    Test Window#to_s

--- test/uh/test_window.rb
+++ test/uh/test_window.rb
@@ -5,6 +5,12 @@ module Uh; class UhWindowSpec < UhSpec
     let(:dpy) { Display.new.tap &:open }
     subject   { described_class.new dpy, dpy.root.id }
 
+    describe '#to_s' do
+      it 'returns the window identifier as a string' do
+        _(subject.to_s).must_match /\d+/
+      end
+    end
+
     describe '#name=' do
       it 'raises an error when given argument is not a string' do
         assert_raises(TypeError) { subject.name = :not_a_string }
