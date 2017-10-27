4ec9c3013e21f2380c4e2a732f69b7dae98d5f55
Thibault Jouan <tj@a13.fr>
2017-10-27 00:12:35 +0000

    Remove comments and notes about minitest/parallel

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,10 @@
-#require 'minitest/parallel'
 require 'minitest/spec'
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'uh'
 
 class Minitest::Test
-  # FIXME: SpecReporter output is incorrect with pretty diffs or parallel tests
   #make_my_diffs_pretty!
-  #parallelize_me!
 end
 
 class UhSpec < Minitest::Spec
